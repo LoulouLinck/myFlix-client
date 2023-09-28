e9e875346ba0389a8fb34526c7f4424b2e94bab1
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-09-28 19:40:34 +0200

    added proptype conditions for MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,6 @@
+// Here you import the PropTypes library
+import PropTypes from "prop-types";
+
 const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,6 +11,28 @@ const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+// Here is where we define all the props constraints for the BookCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    // Imagepath: PropTypes.string.isRequired,
+    // ReleaseYear: PropTypes.string,
+    // Description: PropTypes.string,
+    // Director: PropTypes.shape({
+    //   Name: PropTypes.string,
+    //   Bio: PropTypes.string,
+    //   Birth: PropTypes.string,
+    //   Death: PropTypes.string
+    // }),
+    // Genre: PropTypes.shape({
+    //   Name: PropTypes.string,
+    //   Description: PropTypes.string
+    // }),
+    // Featured: PropTypes.bool,
+    Title: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
 
 export default MovieCard;
