c70f0661afedae557e442a5058a3e81c0732e4f8
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-10-03 20:01:05 +0200

    Fixed syntax

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import MovieCard from "../movie-card/movie-card";
-import MovieView from "../movie-view/movie-view";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 
 
