85e705519ff2296efa77188903f0fb7e8979e171
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-10-04 18:09:24 +0200

    replaced JSX elements w/ Card-related Bootstrap components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,31 +1,50 @@
+import React from "react";
 // Here you import the PropTypes library
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 // The MovieCard function component
-const MovieCard = ({ movie, onMovieClick }) => {
+
+// const MovieCard = ({ movie, onMovieClick }) => {
+//   return (
+//     <div
+//       onClick={() => {
+//         onMovieClick(movie);
+//       }}
+//     >
+//       {movie.Title}
+//     </div>
+//   );
+// };
+
+export const MovieCard = ({ movie, onBookClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
-    </div>
+    <Card>
+      <Card.Img variant="top" src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Director}</Card.Text>
+        <Button onClick={() => onBookClick(book)} variant="link">
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
+
 
 // Here is where we define all the props constraints for the BookCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    // ImagePath: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
     // ReleaseYear: PropTypes.string.isRequired,
     // Description: PropTypes.string.isRequired,
-    // Director: PropTypes.shape({
-    //   Name: PropTypes.string.isRequired,
-    //   Bio: PropTypes.string.isRequired,
-    //   Birth: PropTypes.string.isRequired,
-    //   Death: PropTypes.string.isRequired
-    // }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+      Death: PropTypes.string.isRequired
+    }),
     // Genre: PropTypes.shape({
     //   Name: PropTypes.string.isRequired,
     //   Description: PropTypes.string.isRequired
