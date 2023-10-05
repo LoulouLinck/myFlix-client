bd5897ef9c390d2045701b5ba7ccba19bcbdd3d6
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-10-05 03:34:35 +0200

    Imported bootstrap elements

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from 'react-bootstrap/Container'
 
 export const SignupView = () => {
   const [username, setUsername] = useState("");
