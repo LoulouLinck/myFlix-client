823ca31ed69f39d81c64b2768e6e8ca4725b613d
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-10-05 16:20:29 +0200

    Fixed syntax

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+
+import { MainView } from "./components/main-view/main-view"; // w/ or w/out {} ?
+import Container from "react-bootstrap/Container";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
-import MainView from "./components/main-view/main-view";
-
-
-import Container from "react-bootstrap/Container";
+// Import react-bootstrap library
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
