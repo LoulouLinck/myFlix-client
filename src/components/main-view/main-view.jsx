0877b3681d133b18f296a7da2075da4541220b50
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-10-07 22:08:27 +0200

    Imported and defined NavBar

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 import { ProfileView } from "../profile-view/profile-view";
 // import { Button } from "react-bootstrap";
 import { Row, Col } from "react-bootstrap";
@@ -20,62 +21,52 @@ export const MainView = () => {
   // const [selectedMovie, setSelectedMovie] = useState(null);
   const [token, setToken] = useState(storedToken ? storedToken : null);
 
- useEffect(() => {
-   if (!token) {
-    return;
-   }
-   fetch("https://cineflix-sqlk.onrender.com/movies", {
-     headers: { Authorization: `Bearer ${token}` }
-   })
-     .then((response) => response.json())
-     .then((data) => {
-       console.log(data);
-        const moviesFromApi = data.map((movie) => {
-          return {
-            _id: movie.id, //or movie.key?
-            ImagePath: movie.ImagePath,
-            Title: movie.Title,
-            ReleaseYear: movie.ReleaseYear,
-            Description: movie.Description,
-            Genre: {
-              Name: movie.Genre.Name,
-              Description: movie.Genre.Description
-            },
-            Director: {
-              Name: movie.Director.Name,
-              Bio: movie.Director.Bio,
-              Birth: movie.Director.Birth,
-              Death: movie.Director.Death
-            },
-            Featured: movie.Featured
-          };
-        });
-        setMovies(moviesFromApi);
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    fetch("https://cineflix-sqlk.onrender.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        // const moviesFromApi = data.map((movie) => {
+        //   return {
+        //     _id: movie.id, //or movie.key?
+        //     ImagePath: movie.ImagePath,
+        //     Title: movie.Title,
+        //     ReleaseYear: movie.ReleaseYear,
+        //     Description: movie.Description,
+        //     Genre: {
+        //       Name: movie.Genre.Name,
+        //       Description: movie.Genre.Description
+        //     },
+        //     Director: {
+        //       Name: movie.Director.Name,
+        //       Bio: movie.Director.Bio,
+        //       Birth: movie.Director.Birth,
+        //       Death: movie.Director.Death
+        //     },
+        //     Featured: movie.Featured
+        //   };
+        // });
+        // setMovies(moviesFromApi);
+        setMovies(data);
       });
   }, [token]);
   //returns login view: users have to login to use app
 
   return (
-    <Row className="justify-content-md-center"> 
-      {!user ? (
-           <Col md={5}>
-          <LoginView onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-          }}
-        />
-          or
-          <SignupView />
-          </Col>
-      ) : selectedMovie ? (
-
-        <Col md={8}>
-        <MovieView 
-          movie={selectedMovie} 
-          onBackClick={() => setSelectedMovie(null)} 
-        />
-        </Col>
     <BrowserRouter>
+      <NavigationBar
+        user={user}
+        onLoggedOut={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear();
+        }}
+      />
       <Row className="justify-content-md-center cards-container">
         <Routes>
           <Route
@@ -129,6 +120,19 @@ export const MainView = () => {
             }
           />
 
+          <Route
+            path="/"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is empty!</Col>
+                ) : (
+                  <>
+                    {movies.map((movie) => (
+                      <Col className="mb-5" key={movie.id} md={3}>
+                        <MovieCard
                           movie={movie}
                           user={user}
                           token={token}
@@ -204,10 +208,10 @@ export const MainView = () => {
 //             }}
 //           />
 //       ))}
-//        <button 
-//           onClick={() => { 
-//             setUser(null); 
-//             setToken(null); 
+//        <button
+//           onClick={() => {
+//             setUser(null);
+//             setToken(null);
 //             localStorage.clear();
 //         }}
 //       >
