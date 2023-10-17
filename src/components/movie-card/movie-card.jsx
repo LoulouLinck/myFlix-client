b2b6173317aea1229e974ec5372be317a7fdc341
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-10-17 02:35:15 +0200

    styling

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 // Here you import the PropTypes library
 import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./movie-card.scss";
 
@@ -84,34 +84,57 @@ export const MovieCard = ({ movie, user, token, setUser }) => {
   };
 
   return (
-    <div>
-      <Card className="h-100">
-        <Card.Title>{movie.Title}</Card.Title>
-        {/* <Card.Img  className='w-100' variant="top" src={movie.ImagePath} /> */}
-        <Card.Body>
+  
+      <Card className="border-0 h-100 justify-content-center card">
+        <Card.Title className='card-title' >{movie.Title}</Card.Title>
+        <Card.Img  className='w-100' variant="top" src={movie.ImagePath} />
+        <Card.Body className='card-body'>
           <Card.Text>
             {movie.Director.Name}, {movie.Genre.Name}, {movie.ReleaseYear}
           </Card.Text>
         </Card.Body>
 
-        <Card.Footer>
+        <Card.Footer className='card-footer'>
           <Link to={`/movies/${movie._id}`}>
             <Button className="info-button" variant="light">
               More Info
             </Button>
           </Link>
           {isFavourite ? (
-            <Button variant="danger" onClick={removeFavouriteMovie}>
-              Remove from favourite
+            <Button  className="btn-fav-movie"
+            variant="link" onClick={removeFavouriteMovie}>
+               <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      fill="red"
+                      class="bi bi-heart-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                      />
+                    </svg>
             </Button>
           ) : (
-            <Button variant="primary" onClick={addFavouriteMovie}>
-              Add to favourite
+            <Button   className="btn-fav-movie"
+            variant="link" onClick={addFavouriteMovie}>
+               <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      fill="red"
+                      class="bi bi-heart"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                    </svg>
             </Button>
           )}
         </Card.Footer>
       </Card>
-    </div>
+
   );
 };
 
